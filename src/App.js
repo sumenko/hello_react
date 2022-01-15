6a5a385fcf5e96e5502522bead185303890e3d3c
Vladimir Sumenko <3760097@gmail.com>
2022-01-15 18:21:25 +0300

    add Class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, {useState} from 'react';
-import Counter from './components/Counter';
+import React from 'react';
+import ClassCounter from './components/ClassCounter';
 
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
