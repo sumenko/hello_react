531786dd08de6ea2b018a7103a055dd5046bd7ce
Vladimir Sumenko <3760097@gmail.com>
2022-01-15 11:23:14 +0300

    add input

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 function App() {
   const [likes, setLikess] = useState(5);
-
+  const [value, setValue] = useState('hello world');
   function Increment(){
     setLikess(likes+1);
   }
@@ -12,6 +12,12 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input 
+        type="text"
+        value={value}
+        onChange={evenat => setValue(evenat.target.value)}
+      />
       <button onClick={Increment}>+</button>
       <button onClick={Decrement}>-</button>
     </div>
