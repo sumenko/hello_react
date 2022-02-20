486f75dbce08da529a4ca5642d5ed11a82b4cfe9
Vladimir Sumenko <3760097@gmail.com>
2022-02-20 21:33:42 +0300

    finish transforms

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -14,7 +14,7 @@ const PostList = ({posts, title, remove}) => {
                         {posts.map((post, index) =>
                                     <CSSTransition
                                         key={post.id}
-                                        timeout={500}
+                                        timeout={300}
                                         classNames="post"
                                     >
                                         <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>    
